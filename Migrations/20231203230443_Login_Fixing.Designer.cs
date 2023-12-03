8c85b8ade7357ffc48fb6d7d1d664f7968a09cf5
Žan Reščič <rescic.zan@gmail.com>
2023-12-04 00:12:35 +0100

    dll files

--- Migrations/20231203230443_Login_Fixing.Designer.cs
+++ Migrations/20231203230443_Login_Fixing.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using e_Vstopnice.Data;
 
@@ -11,9 +12,11 @@ using e_Vstopnice.Data;
 namespace e_Vstopnice.Migrations
 {
     [DbContext(typeof(UserContext))]
-    partial class UserContextModelSnapshot : ModelSnapshot
+    [Migration("20231203230443_Login_Fixing")]
+    partial class Login_Fixing
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
