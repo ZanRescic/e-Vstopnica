5d24c813111e3f960c3228288a749a5b289df5e3
Žan Reščič <rescic.zan@gmail.com>
2023-12-05 20:34:08 +0100

    Added roles to db, locked eventController for usrs

--- Controllers/EventController.cs
+++ Controllers/EventController.cs
@@ -7,9 +7,11 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using e_Vstopnice.Data;
 using e_Vstopnice.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace e_Vstopnice.Controllers
 {
+    [Authorize]
     public class EventController : Controller
     {
         private readonly UserContext _context;
@@ -44,6 +46,7 @@ namespace e_Vstopnice.Controllers
         }
 
         // GET: Event/Create
+        [Authorize(Roles = "Administrator, Organizator")]
         public IActionResult Create()
         {
             return View();
@@ -66,6 +69,7 @@ namespace e_Vstopnice.Controllers
         }
 
         // GET: Event/Edit/5
+        [Authorize(Roles = "Administrator, Organizator, Osebje")]
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
@@ -117,6 +121,7 @@ namespace e_Vstopnice.Controllers
         }
 
         // GET: Event/Delete/5
+        [Authorize(Roles = "Administrator, Osebje")]
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
