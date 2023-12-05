3a686d186939c7e9c8065ce1140b1547761aacba
Žan Reščič <rescic.zan@gmail.com>
2023-12-05 14:50:48 +0100

    Fixing _LoginPartial

--- obj/Debug/net8.0/e-Vstopnice.AssemblyInfo.cs
+++ obj/Debug/net8.0/e-Vstopnice.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("e-Vstopnice")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ee8bce53e26bf229b80e66051e2ba1a7623fa65")]
 [assembly: System.Reflection.AssemblyProductAttribute("e-Vstopnice")]
 [assembly: System.Reflection.AssemblyTitleAttribute("e-Vstopnice")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
