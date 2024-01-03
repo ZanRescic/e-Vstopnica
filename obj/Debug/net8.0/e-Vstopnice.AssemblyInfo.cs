58b8de4803483462ab14fb89a0ff67c44a53d011
Žan Reščič <rescic.zan@gmail.com>
2024-01-03 17:21:42 +0100

    Spreminjanje in popravljanje nakupa kart

--- obj/Debug/net8.0/e-Vstopnice.AssemblyInfo.cs
+++ obj/Debug/net8.0/e-Vstopnice.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("e-Vstopnice")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fafa6c69909be26e7da29a26642090f8014ec491")]
 [assembly: System.Reflection.AssemblyProductAttribute("e-Vstopnice")]
 [assembly: System.Reflection.AssemblyTitleAttribute("e-Vstopnice")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
